519321ee5206c8eccaf8c50e2136299d127ebbb6
kader <kader.ergin.37@gmail.com>
2024-09-07 22:42:58 +0300

    Completing_View_Components_on_the_Home_Page

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultDiscountOfDayComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultDiscountOfDayComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
