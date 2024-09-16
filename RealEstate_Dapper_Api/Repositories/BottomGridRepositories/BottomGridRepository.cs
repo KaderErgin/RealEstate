f3c990b06c8db8afd8729b007a9cdf17c68fe4fd
kader <kader.ergin.37@gmail.com>
2024-09-16 23:28:11 +0300

    BottomGrid_Settings

---
+++ RealEstate_Dapper_Api/Repositories/BottomGridRepositories/BottomGridRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
+using RealEstate_Dapper_Api.Dtos.ProductDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.BottomGridRepositories
+{
+    public class BottomGridRepository : IBottomGridRepository
+    {
+        private readonly Context _context;
+
+        public BottomGridRepository(Context context)
+        {
+            _context = context;
+        }
+        public void CreateBottomGrid(CreateBottomGridDto bottomGridDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBottomGrid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultBottomGridDto>> GetAllBottomGrideAsync()
+        {
+            string query = "Select * From BottomGrid";
+            using (var connection = _context.CreateConnection())
+            {
+                var value = await connection.QueryAsync<ResultBottomGridDto>(query);
+                return value.ToList();
+            }
+        }
+
+        public Task<GetBottomGridDto> GetBottomGrid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBottomGrid(UpdateBottomGridDto updateBottomGridDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultBottomGridComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.BottomGridDtos;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultBottomGridComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public _DefaultBottomGridComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44333/api/BottomGrids");//listele
+            if (responseMessage.IsSuccessStatusCode)//islem basariliysa
+            {
+                var jsonData = await
+                responseMessage.Content.ReadAsStringAsync();//gelen icerigi string formatinda oku
+                var values = JsonConvert.DeserializeObject<List<ResultBottomGridDto>>(jsonData);//json formatında veriyi oku tablo(metin) formatına donustur
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
