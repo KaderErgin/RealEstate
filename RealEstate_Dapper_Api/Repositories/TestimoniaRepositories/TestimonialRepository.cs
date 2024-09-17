e596a7beb2182ac999fb971962e57874609a7dbe
kader <kader.ergin.37@gmail.com>
2024-09-18 00:09:37 +0300

    Customer_Comments_Added

---
+++ RealEstate_Dapper_Api/Repositories/TestimoniaRepositories/TestimonialRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.ServiceDtos;
+using RealEstate_Dapper_Api.Dtos.TestimonialDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.TestimoniaRepositories
+{
+    public class TestimonialRepository : ITestimonialRepository
+    {
+        private readonly Context _context;
+
+        public TestimonialRepository(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<ResultTestimonialDto>> GetAllTestimonialeAsync()
+        {
+            string query = "Select * From Testimonial";
+            using (var connection = _context.CreateConnection())
+            {
+                var value = await connection.QueryAsync<ResultTestimonialDto>(query);
+                return value.ToList();
+            }
+        }
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultTestimonialComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.TestimonialDtos;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultTestimonialComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public _DefaultTestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44333/api/Testimonials");//listele
+            if (responseMessage.IsSuccessStatusCode)//islem basariliysa
+            {
+                var jsonData = await
+                responseMessage.Content.ReadAsStringAsync();//gelen icerigi string formatinda oku
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);//json formatında veriyi oku tablo(metin) formatına donustur
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
