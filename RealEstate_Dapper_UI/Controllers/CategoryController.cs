98f1b2ae7876bb0c4e2c3e9811269dcef0cb953a
kader <kader.ergin.37@gmail.com>
2024-09-20 23:50:15 +0300

    Using_Category_API_in_Admin_Panel

--- RealEstate_Dapper_UI/Controllers/CategoryController.cs
+++ RealEstate_Dapper_UI/Controllers/CategoryController.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.CategoryDtos;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public CategoryController   (IHttpClientFactory httpClientFactory)
         {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44333/api/Categories");//listele
+            if (responseMessage.IsSuccessStatusCode)//islem basariliysa
+            {
+                var jsonData = await
+                responseMessage.Content.ReadAsStringAsync();//gelen icerigi string formatinda oku
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);//json formatında veriyi oku tablo(metin) formatına donustur
+                return View(values);
+            }
             return View();
         }
     }
