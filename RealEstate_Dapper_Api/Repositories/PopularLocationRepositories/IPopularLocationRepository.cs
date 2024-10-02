857c626ded4f4ca3386d4461de6066e11c1614a6
kader <kader.ergin.37@gmail.com>
2024-10-02 23:27:12 +0300

    Popular_Locations_API_and_Consume

--- RealEstate_Dapper_Api/Repositories/PopularLocationRepositories/IPopularLocationRepository.cs
+++ RealEstate_Dapper_Api/Repositories/PopularLocationRepositories/IPopularLocationRepository.cs
@@ -5,5 +5,9 @@ namespace RealEstate_Dapper_Api.Repositories.PopularLocationRepositories
     public interface IPopularLocationRepository
     {
         Task<List<ResultPopularLocationDto>> GetAllPopularLocationdeAsync();
+        void CreatePopularLocation(CreatePopularLocationDto PopularLocationDto);
+        void DeletePopularLocation(int id);
+        void UpdatePopularLocation(UpdatePopularLocationDto PopularLocationDto);
+        Task<GetByIDPopularLocationDto> GetPopularLocation(int id); 
     }
 }
---
+++ RealEstate_Dapper_UI/Controllers/PopularLocationController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.PopularLocationDtos;
+using System.Text;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class PopularLocationController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PopularLocationController(IHttpClientFactory httpClientFactory)
+        {
+            this._httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44333/api/PopularLocations");//listele
+            if (responseMessage.IsSuccessStatusCode)//islem basariliysa
+            {
+                var jsonData = await
+                responseMessage.Content.ReadAsStringAsync();//gelen icerigi string formatinda oku
+                var values = JsonConvert.DeserializeObject<List<ResultPopularLocationDto>>(jsonData);//json formatında veriyi oku tablo(metin) formatına donustur
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreatePopularLocation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePopularLocation(CreatePopularLocationDto createPopularLocationDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createPopularLocationDto);//ekle
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");//string dondulecekler (icerigin kendisi,turu(tr destekli),medya tur)
+            var responseMessage = await client.PostAsync("https://localhost:44333/api/PopularLocations", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeletePopularLocation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44333/api/PopularLocations/{id}");
+            if (responseMessage.IsSuccessStatusCode)//200 donerse
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdatePopularLocation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();//ornek alındı
+            var responseMessage = await client.GetAsync($"https://localhost:44333/api/PopularLocations/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdatePopularLocationDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdatePopularLocation(UpdatePopularLocationDto updatePopularLocationDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updatePopularLocationDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync($"https://localhost:44333/api/PopularLocations/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
