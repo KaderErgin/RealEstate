f1e124e5caaef966cdd6a117d47e9f9257095190
kader <kader.ergin.37@gmail.com>
2024-10-07 00:59:42 +0300

    Jwt_Token_Generator_Class

---
+++ RealEstate_Dapper_Api/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Dapper_Api.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "https://localhost"; //Token Kim Tarafindan Bilinecek
+        public const string ValidIssuer = "https://localhost";//Kim Tarafindan Yayinlanacak
+        public const string Key = "REALestate..0102030405Asp.NetCore8.0.1+-*/";
+        public const int Expire= 5;//dk Token
+    }
+}
---
+++ RealEstate_Dapper_Api/Tools/JwtTokenGenerate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RealEstate_Dapper_Api.Tools
+{
+    public class JwtTokenGenerate
+    {
+        public static TokenResponseViewModel GenerateToken(GetCheckAppUserViewModel model)
+        {
+            var claims = new List<Claim>();
+            if (!string.IsNullOrWhiteSpace(model.Role))//kulliciya deger atamasi yapildiysa
+                claims.Add(new Claim(ClaimTypes.Role,model.Role));//rol atamasi yap
+
+            claims.Add(new Claim(ClaimTypes.NameIdentifier,model.Id.ToString()));
+
+            if (!string.IsNullOrWhiteSpace(model.UserName))
+                claims.Add(new Claim("username",model.UserName));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key)); //Token icin basvuru
+            var signinCredantials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);//          kullanilacak yapi algoritma
+            var expireDate = DateTime.UtcNow.AddDays(JwtTokenDefaults.Expire);                //          hayatta kalma suresi
+            JwtSecurityToken token = new JwtSecurityToken(issuer: JwtTokenDefaults.ValidIssuer, audience:
+                JwtTokenDefaults.ValidAudience, claims: claims, notBefore: DateTime.UtcNow, expires:
+                expireDate, signingCredentials: signinCredantials);
+            JwtSecurityTokenHandler tokenHandler=new JwtSecurityTokenHandler();
+            return new TokenResponseViewModel(tokenHandler.WriteToken(token),expireDate);
+        }
+    }
+}
