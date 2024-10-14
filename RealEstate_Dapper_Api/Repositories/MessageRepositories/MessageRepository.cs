781edc783841b5a9afba95c443972b81d7408e25
kader <kader.ergin.37@gmail.com>
2024-10-15 01:05:57 +0300

    Listing_the_Last_3_Messages_in_Real_Estate_Agent_Navbar_UI

--- RealEstate_Dapper_Api/Repositories/MessageRepositories/MessageRepository.cs
+++ RealEstate_Dapper_Api/Repositories/MessageRepositories/MessageRepository.cs
@@ -13,12 +13,14 @@ namespace RealEstate_Dapper_Api.Repositories.MessageRepositories
         }
         public async Task<List<ResultInBoxMessageDto>> GetInBoxLast3MessageListByReceiver(int id)
         {
-            string query = "Select Top(3) * From Message Where Receiver=@receiverId Order By MessageId Desc";
+            string query = "Select Top(3) MessageId,Name,Subject,Detail,SendDate,IsRead,UserImageUrl" +
+                " From Message Inner Join AppUser On Message.Sender=AppUser.UserId " +
+                " Where Receiver=@receiverId Order By MessageId Desc";
             var parameters = new DynamicParameters();
             parameters.Add("@receiverId", id);
             using (var connection = _context.CreateConnection())
             {
-                var values = await connection.QueryAsync<ResultInBoxMessageDto>(query, parameters);//liste donucek
+                var values = await connection.QueryAsync<ResultInBoxMessageDto>(query, parameters);
                 return values.ToList();
             }
         }
